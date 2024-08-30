e9c360e899c0ae3d45354accd0ea6e7e63f65606
Mac452 <mac452@Mac452s-MacBook-Pro.local>
2024-08-30 10:54:45 +0300

    optimize

--- js/app.js
+++ js/app.js
@@ -1,40 +1,44 @@
-const particlesConfig = {
-  particles: {
-    number: {
-      value: window.innerWidth <= 556 ? 0 : 100,
-    },
-    color: {
-      value: "#ff9900",
-    },
-    shape: {
-      type: "circle",
-    },
-    opacity: {
-      value: 0.8,
-    },
-    size: {
-      value: 3,
-    },
-    move: {
-      enable: true,
-      speed: 2,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-    },
-    shadow: {
-      enable: true,
-      color: "#ff9900",
-      blur: 10,
-      offset: {
-        x: 0,
-        y: 0,
+if (window.innerWidth <= 556) {
+  return;
+} else {
+  const particlesConfig = {
+    particles: {
+      number: {
+        value: 100,
+      },
+      color: {
+        value: "#ff9900",
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 0.8,
+      },
+      size: {
+        value: 3,
+      },
+      move: {
+        enable: true,
+        speed: 2,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+      },
+      shadow: {
+        enable: true,
+        color: "#ff9900",
+        blur: 10,
+        offset: {
+          x: 0,
+          y: 0,
+        },
       },
     },
-  },
-  retina_detect: true,
-};
+    retina_detect: true,
+  };
 
-tsParticles.load("tsparticles", particlesConfig);
+  tsParticles.load("tsparticles", particlesConfig);
+}
